f1c5eb8a903d8cc0aff6dc954b12220336b782f7
tnunes <thiagorinu@gmail.com>
2016-12-13 21:56:49 -0200

    removing old structure. It is a gem now

--- mixins/enumerable.rb
+++
@@ -1,9 +0,0 @@
-module Enumerable  
-  def each_relation(&block)
-    each do |item|
-      if block_given?
-        yield
-      end
-    end
-  end
-end
--- mixins/writable.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-
-module Writable
-  def save
-    File.open("./datasets/"+self.id.to_s+".json", 'w'){|f| f.write(self.to_json)}
-  end
-end
